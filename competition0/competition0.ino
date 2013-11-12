5a346d600a9e3076545b7c46c702c4f0c2fe9469
Adam Dunlap <dunlapadam@gmail.com>
2013-11-12 13:41:26 -0800

    Patrick's modifications

--- competition0/competition0.ino
+++ competition0/competition0.ino
@@ -7,6 +7,12 @@ MudduinoBot bot;
 
 bool side;
 
+int RCGWThreshold= 750;//center reflectance sensor grey-white threshold PATRICK MOD
+int RSGWThreshold= 650;//side reflectance sensor grey-white threshold PATRICK MOD
+int RCBGThreshold= 910;//center reflectance sensor black-grey threshold PATRICK MOD
+int RSBGThreshold= 845;//side reflectance sensor black-grey threshold PATRICK MOD
+int location=270;
+
 void setup() {
     Serial.begin(115200);
     bot.begin();
@@ -21,17 +27,19 @@ void setup() {
     Serial.println(side);
 }
 
-bool wrong_color(int seedNum) {
-    return (side && seedNum < 0) || (!side && seedNum > 0);
+bool wrong_color(int seedNumLeft) {
+    return (side && seedNumLeft < 0) || (!side && seedNumLeft > 0);
 }
 
 void loop() {
-    static enum { BEELINE, BEELINE_FIND_CIRCLE, ON_CIRCLE} primary_state = BEELINE;
+    static enum { BEELINE, BEELINE_FIND_CIRCLE, ON_CIRCLE, BLACK_LINE_FOLLOW, FIND_BOX} primary_state = BEELINE;
     static uint8_t secondary_state;
     static unsigned long switchTime = 0;
     static unsigned long timer = 0;
     unsigned long curTime = millis();
     unsigned long timeInState = curTime - switchTime;
+    int rs = bot.getSideReflect();
+    int rc = bot.getCenterReflect();
 
     switch(primary_state) {
     case BEELINE:
@@ -77,9 +85,53 @@ void loop() {
             }
         }
         break;
-    case ON_CIRCLE:
+    case BLACK_LINE_FOLLOW:  ///PATRICK MOD START
         switch(secondary_state) {
         case 0:
+        {
+            // follow blackline
+            static int lastState = 0;
+            int rs = bot.getSideReflect();
+            int rc = bot.getCenterReflect();
+            int state;
+            static unsigned long lastStateChangeTime = 0;
+            if (rs > RSGWThreshold) state = 0;  //using grey-white threshold for stronger contrast
+            else if (rc < RCGWThreshold) state = 1;
+            else state = 2;
+            if (state != lastState) {
+                lastStateChangeTime = curTime;
+            }
+            if (curTime - lastStateChangeTime > 2000) { // We've been in one state for a second
+                secondary_state = 100;
+                lastStateChangeTime = curTime;
+                timer = curTime;
+            }
+            switch (state) {
+            case 0: // side is on black--turn left
+                bot.move(120, -70);
+                break;
+            case 1: // center is on white--turn right
+                bot.move(120, 50);
+                break;
+            case 2:
+                bot.move(120, 0);  //center on black, side on white--go straight
+                break;
+            }
+            bot.setServo(0);
+           
+            lastState = state;
+            break;
+        }
+        case 100: // Probably stuck, back up
+            bot.move(-200);
+            if (curTime - timer > 500){
+                secondary_state = 0;
+                timer = curTime;
+            }
+            break;
+        }
+        break;  //END PATRICK MOD
+    case ON_CIRCLE:
         {
             // follow circle
             static int lastState = 0;
@@ -87,8 +139,8 @@ void loop() {
             int rc = bot.getCenterReflect();
             int state;
             static unsigned long lastStateChangeTime = 0;
-            if (rs < 700) state = 0;
-            else if (rc > 700) state = 1;
+            if (rs < RSGWThreshold) state = 0;
+            else if (rc > RCGWThreshold) state = 1;
             else state = 2;
             if (state != lastState) {
                 lastStateChangeTime = curTime;
@@ -111,11 +163,40 @@ void loop() {
             }
             bot.setServo(0);
 
-            int seedNum = bot.readGC(MudduinoBot::LEFT);
-            if (wrong_color(seedNum) && abs(seedNum) < 5 ) {
+            int seedNumLeft = bot.readGC(MudduinoBot::LEFT);
+           
+            switch(seedNumLeft){   //START PATRICK MOD
+                case 0:
+                    location=45;
+                    break;
+                case 1:
+                    location=135;
+                    break;
+                case 2:
+                    location=225;
+                    break;
+                case 3:
+                    location=315;
+                    break;
+            }  //END PATRICK MOD
+            if (wrong_color(seedNumLeft) && abs(seedNumLeft) < 5 ) {
                 bot.halt();
                 secondary_state = 1;
                 timer = curTime;
+            }
+           
+            int seedNumRight = bot.readGC(MudduinoBot::RIGHT);
+           
+            if (wrong_color(seedNumRight) && abs(seedNumRight)==5) {
+                bot.tone(440);
+            }
+            else
+            {
+              bot.noTone();
+            }
+           
+            if (curTime > 90000 && wrong_color(seedNumRight) && abs(seedNumRight) == 6) {
+                primary_state=FIND_BOX;
             }
 
             lastState = state;
@@ -127,6 +208,13 @@ void loop() {
                 secondary_state = 0;
                 timer = curTime;
             }
+            if((rs>RSGWThreshold)&&(rc>RCGWThreshold)){//START PATRICK MOD--adds turning to backing up if both see grey
+                while(!((rs>RSGWThreshold)&&(rc<RCGWThreshold))){ 
+                    bot.move(0,50);
+                    rs = bot.getSideReflect();
+                    rc = bot.getCenterReflect();
+                }
+            } //END PATRICK MOD
             break;
 
         case 1:
@@ -148,6 +236,82 @@ void loop() {
             break;
         }
         break;
+    case FIND_BOX:///PATRICK MOD START
+        switch(secondary_state) {
+        case 0:
+        {
+            // follow blackline
+            static int lastState = 0;
+
+            int state;
+            static unsigned long lastStateChangeTime = 0;
+            if (rs > RSGWThreshold) state = 0;  //using grey-white threshold for stronger contrast
+            else if (rc < RCGWThreshold) state = 1;
+            else state = 2;
+            if (state != lastState) {
+                lastStateChangeTime = curTime;
+            }
+            if (curTime - lastStateChangeTime > 2000) { // We've been in one state for a second
+                secondary_state = 100;
+                lastStateChangeTime = curTime;
+                timer = curTime;
+            }
+            switch (state) {
+            case 0: // side is on black--turn left
+                bot.move(120, -70);
+                break;
+            case 1: // center is on white--turn right
+                bot.move(120, 50);
+                break;
+            case 2:
+                bot.move(120, 0);  //center on black, side on white--go straight
+                break;
+            }
+           
+            int seedNum = bot.readGC(MudduinoBot::FRONT);
+            if (bot.getBumper() && !(wrong_color(seedNum)) && abs(seedNum)==6) {  //has hit and changed beacon
+                secondary_state = 1;
+            }
+            lastState = state;
+            break;
+        }
+        case 1: //has hit the beacon--turning
+            bot.move(0,-70);
+            int seedNum = bot.readGC(MudduinoBot::FRONT);
+            if (seedNum == 8) {
+                secondary_state=2
+            }
+            break;
+           
+        case 2:  //has acquired gc7, drives forward
+            int rs = bot.getSideReflect();
+            int rc = bot.getCenterReflect();
+            bot.move(120,0);
+            if(((rs<RSGWThreshold)&&(rs>200))||((rc<RCGWThreshold)&&(rc>200))){
+                secondary_state=3;
+              }
+            break;
+        case 3: //has passed yellow once, slows down
+            int rs = bot.getSideReflect();
+            int rc = bot.getCenterReflect();
+            bot.move(70,0);
+            if(((rs<RSGWThreshold)&&(rs>200))||((rc<RCGWThreshold)&&(rc>200))){
+                secondary_state=3;
+               }
+            break;
+        case 4: //has passed yellow twice, stops
+            bot.halt();
+            break;
+        case 100: // Probably stuck, back up
+            bot.move(-200);
+            if (curTime - timer > 500){
+                secondary_state = 0;
+                timer = curTime;
+            }
+            break;
+        }
+       
+        break;  //END PATRICK MOD
+   
     }
 }
-
